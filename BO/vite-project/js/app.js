60aca4ae7daf19611b4fbb63c6624fe86a058908
Leon Westerveld <114667043+leonwesterveld@users.noreply.github.com>
2024-06-03 12:44:06 +0200

    ja

--- BO/vite-project/js/app.js
+++
@@ -1,9 +0,0 @@
-let info = document.getElementById("info");
-let page = document.getElementById("info__page");
-let f = false;
-
-info.onclick = function () {
-    page.style.display = "flex";
-    page.style.zIndex = "3";
-}
-
